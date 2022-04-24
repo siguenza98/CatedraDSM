97488c357c2eaf207133371c3902bfeb9f250615
Fernando Sigüenza <fernando.siguenza.98@gmail.com>
2022-04-23 18:37:43 -0600

    Ejemplo conexion a api
    Actividad con un ejemplo de peticion usando un controlador

--- app/src/main/java/sv/edu/udb/catedradsm/MainActivity.java
+++ app/src/main/java/sv/edu/udb/catedradsm/MainActivity.java
@@ -3,12 +3,28 @@ package sv.edu.udb.catedradsm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import sv.edu.udb.catedradsm.controllers.UserController;
 
 public class MainActivity extends AppCompatActivity {
 
+    UserController userController = new UserController(MainActivity.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userController.realizarPeticion(new UserController.VolleyResponseListener() {
+            @Override
+            public void onError(String mensaje) {
+                Toast.makeText(MainActivity.this, mensaje, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Object respuesta) {
+                Toast.makeText(MainActivity.this, respuesta.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/sv/edu/udb/catedradsm/controllers/UserController.java
@@ -0,0 +1,53 @@
+package sv.edu.udb.catedradsm.controllers;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import sv.edu.udb.catedradsm.models.ConexionModel;
+
+public class UserController {
+    String url = "https://udbdsmapi.000webhostapp.com/api/user";
+    Context context;
+    String respuesta = "";
+
+    public UserController(Context context) {
+        this.context = context;
+    }
+
+    public interface VolleyResponseListener{
+        void onError(String mensaje);
+        void onResponse(Object respuesta);
+    }
+
+    public void realizarPeticion(VolleyResponseListener volleyResponseListener){
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try{
+                    volleyResponseListener.onResponse(response);
+                }catch(Exception e){
+                    Toast.makeText(context, "Hubo un error al realizar la petición", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                volleyResponseListener.onError("Hubo un error al realizar la petición.");
+            }
+
+        });
+        ConexionModel.getInstance(context).addToRequestQueue(request);
+    }
+}
