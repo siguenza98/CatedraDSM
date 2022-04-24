1cbf07a1adc6a259fad2958771ea718128bb5289
Fernando Sigüenza <fernando.siguenza.98@gmail.com>
2022-04-24 15:06:37 -0600

    metodo login

--- app/src/main/java/sv/edu/udb/catedradsm/Login.java
+++ app/src/main/java/sv/edu/udb/catedradsm/Login.java
@@ -3,14 +3,22 @@ package sv.edu.udb.catedradsm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import sv.edu.udb.catedradsm.controllers.UserController;
+import sv.edu.udb.catedradsm.models.UserModel;
 
 public class Login extends AppCompatActivity {
     UserController userController = new UserController(Login.this);
     private EditText txtCorreo, txtPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +27,25 @@ public class Login extends AppCompatActivity {
 
         txtCorreo = (EditText)findViewById(R.id.txtCorreo);
         txtPassword = (EditText)findViewById(R.id.txtPassword);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
 
+
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String correo = txtCorreo.getText().toString();
+                String pass = txtPassword.getText().toString();
+
+                if(correo != "" && correo.length() != 0 && pass != "" && pass.length() != 0){
+                    iniciarSesion(correo, pass);
+                }else{
+                    Toast.makeText(Login.this, "Ingrese sus credenciales.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    public void iniciarSesion(){
-        String correo = txtCorreo.getText().toString();
-        String pass = txtPassword.getText().toString();
+    public void iniciarSesion(String correo, String pass){
 
         userController.iniciarSesion(correo, pass, new UserController.VolleyResponseListener() {
             @Override
--- app/src/main/java/sv/edu/udb/catedradsm/controllers/UserController.java
+++ app/src/main/java/sv/edu/udb/catedradsm/controllers/UserController.java
@@ -1,6 +1,7 @@
 package sv.edu.udb.catedradsm.controllers;
 
 import android.content.Context;
+import android.util.Base64;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -8,11 +9,15 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import sv.edu.udb.catedradsm.models.ConexionModel;
 
@@ -52,25 +57,55 @@ public class UserController {
     }
 
     public void iniciarSesion(String correo, String pass, VolleyResponseListener volleyResponseListener){
-        String apiUrl = url + "/iniciarSesion";
-        JsonArrayRequest request = new JsonArrayRequest(Request.Method.POST, apiUrl, null,new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray response) {
-                try{
-                    volleyResponseListener.onResponse(response);
-                }catch(Exception e){
-                    Toast.makeText(context, "Hubo un error al realizar la petición", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                volleyResponseListener.onError("Hubo un error al realizar la petición.");
-            }
+        String apiUrl ="https://udbdsmapi.000webhostapp.com/login";
+        pass = sha256(pass);
 
-        });
-        ConexionModel.getInstance(context).addToRequestQueue(request);
+        JSONObject datos = new JSONObject();
+        try {
+            datos.put("correo", correo);
+            datos.put("pass", pass);
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, apiUrl, datos,new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try{
+
+                        volleyResponseListener.onResponse(response);
+                    }catch(Exception e){
+                        Toast.makeText(context, "Hubo un error al realizar la petición", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    volleyResponseListener.onError("Hubo un error al realizar la petición.");
+                }
+
+            });
+            ConexionModel.getInstance(context).addToRequestQueue(request);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
+    public static String sha256(final String base) {
+        try{
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch(Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
 
 }
