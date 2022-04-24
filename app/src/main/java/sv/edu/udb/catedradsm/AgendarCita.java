99a30a0b9f52e9688fffd5caf9aa7246ee63f256
fm150663 <ded.flores97@gmail.com>
2022-04-24 15:42:25 -0600

    Vistas agregadas y textos desde archivo strings.xml

---
+++ app/src/main/java/sv/edu/udb/catedradsm/AgendarCita.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.catedradsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgendarCita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agendar_cita);
+    }
+}
