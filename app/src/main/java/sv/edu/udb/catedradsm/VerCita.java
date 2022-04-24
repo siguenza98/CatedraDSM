5d3d2663bb4607a6ccef57621bcc69136be02358
fm150663 <ded.flores97@gmail.com>
2022-04-24 15:42:25 -0600

    Vistas agregadas y textos desde archivo strings.xml

---
+++ app/src/main/java/sv/edu/udb/catedradsm/VerCita.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.catedradsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerCita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_cita);
+    }
+}
