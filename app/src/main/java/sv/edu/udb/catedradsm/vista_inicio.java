e88fd14f030a1758163ade3b1f4a5a2b26265a07
Jose Aguilar <JAAL0254@gmail.com>
2022-04-24 20:42:17 -0600

    Vista de pantalla de inicio

---
+++ app/src/main/java/sv/edu/udb/catedradsm/vista_inicio.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.catedradsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class vista_inicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista_inicio);
+    }
+}
